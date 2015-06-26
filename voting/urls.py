7f3e52c671f0674754e949473c61873c88819515
George Hickman <george@ghickman.co.uk>
2015-06-26 21:40:59 +0100

    Set up static serving in dev

--- voting/urls.py
+++ voting/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
 
@@ -15,4 +17,4 @@ urlpatterns = [
     url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
     url(r'^register/$', Register.as_view(), name='register'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
