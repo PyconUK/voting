7341687b2e2e7ddb003cb36c8229da197b475a03
George Hickman <george@ghickman.co.uk>
2015-07-20 15:26:38 +0100

    Fix missing import

--- voting/urls.py
+++ voting/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import RedirectView
 
-from .views import Home, Login, ProposalDetail, ProposalVote, ReviewedProposals
+from .views import (Home, Login, ProposalDetail, ProposalVote,
+                    ReviewedProposals, UnreviewedProposals)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
