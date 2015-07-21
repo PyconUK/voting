9968683b2631c19a705a9dcecdbe604f7df3abdc
George Hickman <george@ghickman.co.uk>
2015-07-21 21:32:01 +0100

    Fail fast and don't return the proposal counts if user is unauthed

--- voting/context_processors.py
+++ voting/context_processors.py
@@ -2,6 +2,9 @@ from .models import Proposal
 
 
 def proposal_counters(request):
+    if not request.user.is_authenticated():
+        return {}
+
     proposals = Proposal.objects.all()
     total = proposals.count()
     remaining = proposals.exclude(vote__user=request.user).count()
